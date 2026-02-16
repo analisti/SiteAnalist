e81e796df46655fe6651dcb1eaf7b1b224eeff3d
Analist <104594528+analisti@users.noreply.github.com>
2026-02-16 15:58:02 -0300

    v1-reform

--- js/custom.js
+++ js/custom.js
@@ -3,3 +3,17 @@
  * Scripts específicos ou experimentais
  * NÃO colocar lógica principal aqui
  */
+const reveals = document.querySelectorAll('.reveal');
+
+function revealOnScroll() {
+  const trigger = window.innerHeight * 0.85;
+
+  reveals.forEach(el => {
+    const top = el.getBoundingClientRect().top;
+    if (top < trigger) {
+      el.classList.add('active');
+    }
+  });
+}
+
+window.addEventListener('scroll', revealOnScroll);
